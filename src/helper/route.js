c9e8b20f29ea95b6956be2f810e8ed258d037999
wangyiming <wangyimig@icloud.com>
2018-01-22 00:45:06 +0800

    玩成静态服务器全部功能

--- src/helper/route.js
+++ src/helper/route.js
@@ -1,24 +1,47 @@
 const fs = require('fs');
+const path = require('path');
 const promisify = require('util').promisify;
 const stat = promisify(fs.stat);
 const readdir = promisify(fs.readdir);
+
+// 模板文件夹所在地址
+const tplPath = path.join(__dirname, '../template');
+
+const source = fs.readFileSync(path.join(tplPath, 'dir.html')).toString();
+// 模板引擎
+const Handlebars = require('handlebars');
+
+const conf = require('../config/defaultConfig');
+
+const mime = require('../helper/mime');
+
+// 文件夹目录显示模板
+const dirTemplate = Handlebars.compile(source);
 
 module.exports = async function (req, res, filePath) {
     try {
         const stats = await stat(filePath);
         if (stats.isFile()) {
             res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/plain; charset=UTF-8');
+            const mimeType = mime(filePath);
+            res.setHeader('Content-Type', `${mimeType}; charset=UTF-8`);
             fs.createReadStream(filePath).pipe(res);
         } else if (stats.isDirectory()) {
             const files = await readdir(filePath);
             res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/plain; charset=UTF-8');
-            res.end(files.join('    '));
+            res.setHeader('Content-Type', 'text/html; charset=UTF-8');
+            const dir = path.relative(conf.root, filePath);
+            const data = {
+                title: path.basename(filePath),
+                dir: dir ? `/${dir}` : '',
+                files
+            };
+            res.end(dirTemplate(data));
         }
     } catch (error) {
+        console.info(error);
         res.statusCode = 404;
         res.setHeader('Content-Type', 'text/plain; charset=UTF-8');
-        res.end('找不到这个地址');
+        res.end(`${filePath} 这个地址找不到\n 错误信息: \n${error}`);
     }
 };
